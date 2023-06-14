044aee53094645837c112f31f11439d7dc90f359
schnuddels <stephan_fechner@hotmail.de>
2023-06-14 15:43:25 +0200

    Inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header class="background-top"></header>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,400;0,500;0,700;1,400&display=swap');
+
+/* -----------------------------------------------------------Basic Styling */
+body {
+  background-color: #f2f2f2;
+  font-family: 'Barlow Semi Condensed', sans-serif;
+  color: #0E0819;
 }
 
-#nav {
-  padding: 30px;
+/* -----------------------------------------------------------Typography */
+h1 {
+  font-weight: 700;
+  font-size: 34px;
+  line-height: 34px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+
+h2 {
+  font-weight: 700;
+  font-size: 21px;
+  line-height: 21px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+h3 {
+  font-weight: 400;
+  font-size: 21px;
+  line-height: 21px;
 }
-</style>
+
+h4 {
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+p {
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+label {
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 13px;
+}
+
+.fontLila {
+  color: #6638B6;
+}
+
+.fontWhite {
+  color: white;
+}
+
+/* -----------------------------------------------------------Header Background */
+.background-top {
+  position: absolute;
+  box-sizing: border-box;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: 65vh;
+  background: linear-gradient(135deg, #05C3DE 5%, #6638B6 35%, #2D0764 85%);
+}</style>
