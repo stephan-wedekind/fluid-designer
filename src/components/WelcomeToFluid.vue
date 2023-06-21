d8f311a22a3103ec26f09051e61f6b9ef45d32b7
schnuddels <stephan_fechner@hotmail.de>
2023-06-21 23:28:25 +0200

    Style Auswählen Bild hinzugefügt mit Link zu Style Comp

--- src/components/WelcomeToFluid.vue
+++ src/components/WelcomeToFluid.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="padding-60">
-    <h1 class="fontPlaceholder">WelcomeToFluid</h1>
+    <div class="chooseStyle">
+      <img src="Platzhalter/Tutorial/maple.png" alt="" @click="setActiveNavigation('style')">
+      <h1 class="fontLila">Wähle einen der drei Styles</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex';
+import { mapMutations } from 'vuex';
 
 export default {
   name: 'WelcomeToFluid',
 
-  computed: {
-    ...mapState(['imagePath']),
+  methods: {
+    ...mapMutations(['setActiveNavigation']),
   },
-
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.chooseStyle {
+  text-align: center;
+}
+.chooseStyle img{
+  width: 100%;
+  cursor: pointer;
+}
+
+.chooseStyle h1 {
+  font-weight: 500;
+  margin-top: 30px;
+}
+</style>
